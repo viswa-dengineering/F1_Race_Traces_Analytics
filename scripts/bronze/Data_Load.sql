d2fc02d48f9b6a1a2f5446c785e61a1af0f156a7
VISWA B <gvish2025@gmail.com>
2026-09-02 13:03:54 +0530

    Add data loading SQL for bronze tables
    Added SQL commands to load data from CSV files into bronze tables for drivers, races, constructors, results, and circuits.

--- scripts/bronze/Data_Load.sql
+++ scripts/bronze/Data_Load.sql
@@ -1,32 +1,46 @@
--- Example: Load drivers.csv
+/* ==========================================
+   LOAD DATA INTO BRONZE TABLES
+   ========================================== */
+
+-- =========================
+-- DRIVERS
+-- =========================
 COPY bronze.drivers
 FROM 'C:\Program Files\PostgreSQL\18\data\archive\drivers.csv'
 DELIMITER ','
 CSV HEADER
 NULL '\N';
 
--- Example: Load races.csv
+-- =========================
+-- RACES
+-- =========================
 COPY bronze.races
 FROM 'C:\Program Files\PostgreSQL\18\data\archive\races.csv'
 DELIMITER ','
 CSV HEADER
 NULL '\N';
 
--- Example: Load constructors.csv
+-- =========================
+-- CONSTRUCTORS
+-- =========================
 COPY bronze.constructors
 FROM 'C:\Program Files\PostgreSQL\18\data\archive\constructors.csv'
 DELIMITER ','
 CSV HEADER
 NULL '\N';
 
--- Example: Load results.csv
+-- =========================
+-- RESULTS
+-- =========================
 COPY bronze.results
 FROM 'C:\Program Files\PostgreSQL\18\data\archive\results.csv'
 DELIMITER ','
 CSV HEADER
 NULL '\N';
 
--- Example: Load circuits.csv
+-- =========================
+-- CIRCUITS
+-- =========================
 COPY bronze.circuits
 FROM 'C:\Program Files\PostgreSQL\18\data\archive\circuits.csv'
 DELIMITER ','
